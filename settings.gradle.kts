d80f6024168d6d028ab45a8cad3523400b169724
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2022-07-09 22:28:56 +0700

    style: format

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,33 +1,33 @@
 arrayOf("gradle.properties", "gradle").forEach(::copyToBuildSrc)
 
 pluginManagement {
-    repositories {
-        google()
-        gradlePluginPortal()
-        mavenCentral()
-    }
+  repositories {
+    google()
+    gradlePluginPortal()
+    mavenCentral()
+  }
 }
 
-rootProject.name = "My_Application"
+rootProject.name = "GithubSearchKMM"
 include(":androidApp")
 include(":shared")
 
 fun includeProject(name: String, filePath: String) {
-    include(name)
-    project(name).projectDir = File(filePath)
+  include(name)
+  project(name).projectDir = File(filePath)
 }
 
 fun copyToBuildSrc(sourcePath: String) {
-    rootDir.resolve(sourcePath).copyRecursively(
-        target = rootDir.resolve("buildSrc").resolve(sourcePath),
-        overwrite = true
-    )
-    rootDir.resolve(sourcePath).copyRecursively(
-        target = rootDir.resolve("buildSrc")
-            .resolve("buildSrc")
-            .resolve(sourcePath),
-        overwrite = true
-    )
-    println("[DONE] copied $sourcePath")
+  rootDir.resolve(sourcePath).copyRecursively(
+    target = rootDir.resolve("buildSrc").resolve(sourcePath),
+    overwrite = true
+  )
+  rootDir.resolve(sourcePath).copyRecursively(
+    target = rootDir.resolve("buildSrc")
+      .resolve("buildSrc")
+      .resolve(sourcePath),
+    overwrite = true
+  )
+  println("[DONE] copied $sourcePath")
 }
 
