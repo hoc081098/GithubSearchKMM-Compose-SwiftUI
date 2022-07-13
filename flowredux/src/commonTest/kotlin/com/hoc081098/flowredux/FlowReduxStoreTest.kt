4d2063594f5f8b00b3be4c72df0148b266fede8e
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2022-07-13 15:48:02 +0700

    test(flow-redux): tests

--- flowredux/src/commonTest/kotlin/com/hoc081098/flowredux/FlowReduxStoreTest.kt
+++ flowredux/src/commonTest/kotlin/com/hoc081098/flowredux/FlowReduxStoreTest.kt
@@ -15,6 +15,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.toList
@@ -25,6 +26,16 @@ import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
+
+@Suppress("NOTHING_TO_INLINE")
+object M {
+  inline operator fun <K, V> get(vararg elements: Pair<K, V>) = elements.toMap()
+}
+
+@Suppress("NOTHING_TO_INLINE")
+object L {
+  inline operator fun <T> get(vararg elements: T): List<T> = elements.asList()
+}
 
 @ExperimentalCoroutinesApi
 private fun TestScope.createScope() = CoroutineScope(
@@ -111,7 +122,6 @@ class FlowReduxStoreTest {
         SideEffect { actions, _ ->
           actions.flatMapConcat {
             sideEffect1Actions += it
-            println("Side effect $it")
             emptyFlow()
           }
         }
@@ -147,6 +157,149 @@ class FlowReduxStoreTest {
     assertContentEquals(
       sideEffect1Actions,
       allActions
+    )
+    scope.cancel()
+  }
+
+  @Test
+  fun `store with 2 side effects and they emit nothing`() = runTest {
+    val scope = createScope()
+    val sideEffect1Actions = mutableListOf<String>()
+    val sideEffect2Actions = mutableListOf<String>()
+
+    val store = scope.createFlowReduxStore<String, String>(
+      initialState = "",
+      sideEffects = listOf(
+        SideEffect { actions, _ ->
+          actions.flatMapConcat {
+            sideEffect1Actions += it
+            emptyFlow()
+          }
+        },
+        SideEffect { actions, _ ->
+          actions.flatMapConcat {
+            sideEffect2Actions += it
+            emptyFlow()
+          }
+        }
+      ),
+      reducer = { state, action ->
+        state + action
+      }
+    )
+    val allActions = mutableListOf<String>()
+    scope.launch(start = CoroutineStart.UNDISPATCHED) {
+      store.actionSharedFlow.toList(allActions)
+    }
+
+    scope.launch {
+      delay(100)
+      store.dispatch("1")
+      store.dispatch("2")
+    }
+
+    store.stateFlow.take(3).testWithTestCoroutineScheduler {
+      assertEquals("", awaitItem())
+      assertEquals("1", awaitItem())
+      assertEquals("12", awaitItem())
+      awaitComplete()
+    }
+    advanceUntilIdle()
+    runCurrent()
+
+    assertContentEquals(
+      listOf("1", "2"),
+      allActions
+    )
+    assertContentEquals(
+      sideEffect1Actions,
+      allActions
+    )
+    assertContentEquals(
+      sideEffect2Actions,
+      allActions
+    )
+    scope.cancel()
+  }
+
+  @Test
+  fun `store with 2 simple side effects`() = runTest {
+    val scope = createScope()
+    val sideEffect1Actions = mutableListOf<Int>()
+    val sideEffect2Actions = mutableListOf<Int>()
+
+    val store = scope.createFlowReduxStore<Int, String>(
+      initialState = "",
+      sideEffects = listOf(
+        SideEffect { actions, _ ->
+          actions.flatMapConcat {
+            sideEffect1Actions += it
+
+            if (it < 6) {
+              flowOf(6)
+            } else {
+              emptyFlow()
+            }
+          }
+        },
+        SideEffect { actions, _ ->
+          actions.flatMapConcat {
+            sideEffect2Actions += it
+
+            if (it < 6) {
+              flowOf(7)
+            } else {
+              emptyFlow()
+            }
+          }
+        }
+      ),
+      reducer = { state, action ->
+        state + action
+      }
+    )
+    val allActions = mutableListOf<Int>()
+    scope.launch(start = CoroutineStart.UNDISPATCHED) {
+      store.actionSharedFlow.toList(allActions)
+    }
+
+    scope.launch {
+      delay(100)
+      store.dispatch(1)
+      delay(100)
+      store.dispatch(2)
+    }
+
+    store.stateFlow.take(7).testWithTestCoroutineScheduler {
+      // Initial State emission
+      assertEquals("", awaitItem())
+
+      // emission of 1
+      assertEquals("1", awaitItem())
+      assertEquals("16", awaitItem())
+      assertEquals("167", awaitItem())
+
+      // emission of 2
+      assertEquals("1672", awaitItem())
+      assertEquals("16726", awaitItem())
+      assertEquals("167267", awaitItem())
+
+      awaitComplete()
+    }
+    advanceUntilIdle()
+    runCurrent()
+
+    assertContentEquals(
+      L[1, 6, 7, 2, 6, 7],
+      allActions
+    )
+    assertContentEquals(
+      L[1, 6, 7, 2, 6, 7],
+      sideEffect1Actions
+    )
+    assertContentEquals(
+      L[1, 6, 7, 2, 6, 7],
+      sideEffect2Actions
     )
     scope.cancel()
   }
