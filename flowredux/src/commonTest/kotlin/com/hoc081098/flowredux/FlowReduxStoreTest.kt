1fab5a35e566bb85174369bdd70b4408e0b23789
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2022-07-13 15:33:57 +0700

    feat(flow-redux): create :flowredux module

--- flowredux/src/commonTest/kotlin/com/hoc081098/flowredux/FlowReduxStoreTest.kt
+++ flowredux/src/commonTest/kotlin/com/hoc081098/flowredux/FlowReduxStoreTest.kt
@@ -8,10 +8,13 @@ import kotlin.test.assertEquals
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.flow.toList
@@ -19,11 +22,18 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.TestCoroutineScheduler
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runCurrent
 import kotlinx.coroutines.test.runTest
 
 @ExperimentalCoroutinesApi
-private fun TestScope.createScope() = CoroutineScope(testScheduler)
+private fun TestScope.createScope() = CoroutineScope(
+  UnconfinedTestDispatcher(
+    testScheduler
+  )
+)
 
+@FlowPreview
 @ExperimentalCoroutinesApi
 class FlowReduxStoreTest {
   @Test
@@ -45,6 +55,8 @@ class FlowReduxStoreTest {
         assertEquals(0, awaitItem())
         awaitComplete()
       }
+    advanceUntilIdle()
+    runCurrent()
 
     assertEquals(0, reducerInvocations)
     scope.cancel()
@@ -78,9 +90,62 @@ class FlowReduxStoreTest {
       assertEquals("12", awaitItem())
       awaitComplete()
     }
+    advanceUntilIdle()
+    runCurrent()
 
     assertContentEquals(
       listOf("1", "2"),
+      allActions
+    )
+    scope.cancel()
+  }
+
+  @Test
+  fun `store with empty side effect that emits nothing`() = runTest {
+    val scope = createScope()
+    val sideEffect1Actions = mutableListOf<String>()
+
+    val store = scope.createFlowReduxStore<String, String>(
+      initialState = "",
+      sideEffects = listOf(
+        SideEffect { actions, _ ->
+          actions.flatMapConcat {
+            sideEffect1Actions += it
+            println("Side effect $it")
+            emptyFlow()
+          }
+        }
+      ),
+      reducer = { state, action ->
+        state + action
+      }
+    )
+    val allActions = mutableListOf<String>()
+    scope.launch(start = CoroutineStart.UNDISPATCHED) {
+      store.actionSharedFlow.toList(allActions)
+    }
+
+    scope.launch {
+      delay(100)
+      store.dispatch("1")
+      store.dispatch("2")
+    }
+
+    store.stateFlow.take(3).testWithTestCoroutineScheduler {
+      assertEquals("", awaitItem())
+      assertEquals("1", awaitItem())
+      assertEquals("12", awaitItem())
+      awaitComplete()
+    }
+    advanceUntilIdle()
+    runCurrent()
+
+    assertContentEquals(
+      listOf("1", "2"),
+      allActions
+    )
+    assertContentEquals(
+      sideEffect1Actions,
       allActions
     )
     scope.cancel()
