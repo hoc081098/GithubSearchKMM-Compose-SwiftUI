2659ef98e311054f6fa4b1fb28680022d0cabfae
Petrus Nguyen Thai Hoc <hoc081098@gmail.com>
2022-10-05 15:43:17 +0700

    fix: opt-in material3 api

--- androidApp/src/main/java/com/hoc081098/github_search_kmm/android/ui/GithubSearchTermBox.kt
+++ androidApp/src/main/java/com/hoc081098/github_search_kmm/android/ui/GithubSearchTermBox.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -26,6 +27,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun GithubSearchTermBox(
   modifier: Modifier = Modifier,
